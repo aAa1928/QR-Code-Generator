b859a198d2de5567b3a59963e496c14f286fc5a1
Risheet Lenka <risheetlenka01@gmail.com>
2025-01-15 20:19:24 -0800

    Added default img and basic customization options on webpage

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 from flask import Flask, render_template
+from qrcode.main import QRCode
+from qrcode.constants import ERROR_CORRECT_L
+
+def generate_qrcode(data: str, *, box_size: int = 10, fill_color: str | tuple = 'black', back_color: str | tuple = 'white'):
+    border_size: int = 2
+    version: int = 1
+
+    qr = QRCode(version=version,
+                error_correction=ERROR_CORRECT_L,
+                box_size=box_size,
+                border=border_size)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=fill_color, back_color=back_color)
+    img.save('qrcode.png')
 
 app = Flask(__name__)
 
