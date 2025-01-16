fcf901b5247c673b5267a15b5b41d2e5d065c77e
Risheet Lenka <risheetlenka01@gmail.com>
2025-01-15 21:20:16 -0800

    Created all functionality needed for program

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from time import time
+
+from flask import Flask, render_template, request
 from qrcode.main import QRCode
 from qrcode.constants import ERROR_CORRECT_L
 
@@ -13,13 +15,27 @@ def generate_qrcode(data: str, *, box_size: int = 10, fill_color: str | tuple = 
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color=fill_color, back_color=back_color)
-    img.save('qrcode.png')
+    img.save('static/images/qrcode.png')
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    timestamp = int(time())
+    qr_generated = False
+
+    if request.method == 'POST':
+        url = request.form.get('url')
+        fill_color = request.form.get('fillColor')
+        back_color = request.form.get('bgColor')
+        box_size = int(request.form.get('boxSize', 10))
+        
+        generate_qrcode(url, box_size=box_size, 
+                       fill_color=fill_color, 
+                       back_color=back_color)
+        qr_generated = True
+
+    return render_template('index.html', timestamp=timestamp, qr_generated=qr_generated)
 
 if __name__ == '__main__':
     app.run()
